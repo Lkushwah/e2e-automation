e3e6ba602cc76f475eb905701e863b5355c5c34c
Laharsh Kushwah <laharsh.kushwah@ubisoft.com>
2026-01-30 16:33:44 +0530

    Initial commit

---
+++ src/test/java/tests/BuildValidationTest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import core.models.Build;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+public class BuildValidationTest {
+    @Test
+    public void testBuildValidation() throws Exception{
+        ObjectMapper mapper = new ObjectMapper();
+        File file = new File("src/test/resources/builds.json");
+
+        List<Build> builds = Arrays.asList(mapper.readValue(file, Build[].class));
+
+        builds.stream()
+                .filter(b->)
+    }
+}
